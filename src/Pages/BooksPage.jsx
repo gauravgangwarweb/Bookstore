b43f2737477e9fc2e4a1e8a15c70aeaec678d65f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-19 22:09:36 +0530

    Add the required changes

--- src/Pages/BooksPage.jsx
+++ src/Pages/BooksPage.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import BooksList from '../components/BooksList';
 import AddBookForm from '../components/AddBookForm';
 
-const BooksPage = () => {
-    return (
-        <div className='border-2 border-black mt-5'>
-            <BooksList />
-            <AddBookForm />
-        </div>
-    )
-}
+const BooksPage = () => (
+    <div className='border-2 border-black mt-5'>
+        <BooksList />
+        <AddBookForm />
+    </div>
+)
 
 export default BooksPage;
