e77acd2970bde97d965040de027e1130d26631c5
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-19 20:31:36 +0530

    Add categories page

---
+++ src/Pages/CategoriesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoriesPage = () => {
+    return (
+        <div>
+            <button className='border-2 border-black' type='button'>Check status</button>
+        </div>
+    )
+}
+
+export default CategoriesPage;
