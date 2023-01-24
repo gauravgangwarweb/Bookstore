3747a7d218a7b635c3c02b46e053aecc83ee54d5
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-24 23:43:23 +0530

    Moidfy AddBookForm file to add addBook action

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
+import { addBook } from '../redux/books/books';
 
 const AddBookForm = () => {
   const dispatch = useDispatch();
+  const [book, setBook] = useState({});
 
   return (
     <div className="p-5">
       <form
         className="flex gap-3"
         onSubmit={(e) => {
-          dispatch({
-            title: e.target.elements.title.value,
-            author: e.target.elements.author.value,
-          });
+          e.preventDefault();
+          dispatch(addBook(book));
+          e.target.title.value = '';
+          e.target.author.value = '';
         }}
       >
-        <input className="border-2 border-black" name="title" type="text" placeholder="Input title" />
-        <input className="border-2 border-black" name="author" type="text" placeholder="Input author" />
+        <input className="border-2 border-black" name="title" type="text" placeholder="Input title" onChange={(e) => setBook({ ...book, title: e.target.value, id: uuid() })} />
+        <input className="border-2 border-black" name="author" type="text" placeholder="Input author" onChange={(e) => setBook({ ...book, author: e.target.value })} />
         <input className="border-2 border-black" type="submit" />
       </form>
     </div>
   );
 };
-
+// onClick={() => {dispatch(addBook(book))}}
 export default AddBookForm;
