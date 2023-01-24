56955012c50d887f6cb9a10108df61ea2fdc60df
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-24 15:53:17 +0530

    Modify AddBookForm file to add reducers

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-const AddBookForm = () => (
-  <div className="p-5">
-    <form className="flex gap-3">
-      <input className="border-2 border-black" type="text" placeholder="Input title" />
-      <input className="border-2 border-black" type="text" placeholder="Input author" />
-      <input className="border-2 border-black" type="submit" />
-    </form>
-  </div>
-);
+const AddBookForm = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="p-5">
+      <form
+        className="flex gap-3"
+        onSubmit={(e) => {
+          dispatch({
+            title: e.target.elements.title.value,
+            author: e.target.elements.author.value,
+          });
+        }}
+      >
+        <input className="border-2 border-black" name="title" type="text" placeholder="Input title" />
+        <input className="border-2 border-black" name="author" type="text" placeholder="Input author" />
+        <input className="border-2 border-black" type="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default AddBookForm;
