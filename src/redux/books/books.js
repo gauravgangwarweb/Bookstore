dbb4a816a08c1fc410844e2408b6dadcb8742089
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-26 21:34:37 +0530

    Modify books js file to add fetch methods

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,36 +1,49 @@
+import { deleteData, getData, sendData } from './fetchBooks';
+
 const ADD_BOOK = 'booksStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const initialState = [
-  {
-    title: 'Book 1',
-    author: 'Author 1',
-    id: 1,
-  },
-  {
-    title: 'Book 2',
-    author: 'Author 2',
-    id: 2,
-  },
-  {
-    title: 'Book 3',
-    author: 'Author 3',
-    id: 3,
-  },
-];
+const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS';
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CZDXQb2zMgHNbimDk0mO/books';
+
+const initialState = [];
+
+export const fetchBooks = (payload) => ({
+  type: FETCH_BOOKS,
   payload,
 });
 
-export const removeBook = (payload) => ({ type: REMOVE_BOOK, id: payload });
+export const addBook = (payload) => async (dispatch) => {
+  await sendData(URL, payload);
+  return dispatch({ type: ADD_BOOK, payload });
+};
+
+export const removeBook = (id) => async (dispatch) => {
+  await deleteData(URL, id);
+  return dispatch({ type: REMOVE_BOOK, payload: id });
+};
+
+export const displayBooks = () => async (dispatch) => {
+  const books = getData(URL);
+  const booksArr = [];
+  Object.keys(books).forEach((id) => {
+    booksArr.push({
+      item_id: id,
+      title: books[id][0].title,
+      author: books[id][0].author,
+    });
+  });
+  dispatch(fetchBooks(booksArr));
+};
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
+      return state.filter((book) => book.id !== action.payload);
+    case FETCH_BOOKS:
+      return action.payload;
     default:
       return state;
   }
