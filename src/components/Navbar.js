ebca067a6e2123d9cb633a01763d1f9193ad9e32
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-28 00:56:34 +0530

    Modify Navbar js file to add styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div className="w-[100%] flex bg-black h-10 items-center px-10 gap-5">
-    <h4 className="text-red-600 text-right font-semibold text-[18px]">TC Bookstore</h4>
-    <div className="flex gap-4 text-white">
-      <Link className="active:text-red-600" to="/">Books</Link>
-      <Link className="active:text-red-600" to="/categories">Categories</Link>
+  <div className="navbar flex justify-between items-center px-10">
+    <div className="w-[100%] flex bg-[#ffffff] h-24 items-center gap-10">
+      <h4 className="text-[#0290ff] text-right text-[30px] font-extrabold">TC Bookstore</h4>
+      <div className="flex gap-4 text-[18px]">
+        <Link className="active:text-red-600" to="/">Books</Link>
+        <Link className="active:text-red-600" to="/categories">Categories</Link>
+      </div>
     </div>
+    <button className="border-2 user-button p-3 rounded-full" type="button">
+      <i className="fa-solid fa-user" />
+      {/* hh */}
+    </button>
   </div>
 );
 
